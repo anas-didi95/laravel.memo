cdaa3f028bc9de7d37512588711411d44f2ceea4
Anas Juwaidi <anas.didi95@gmail.com>
2017-08-07 16:42:12 +0800

    setup laracasts/generators

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -176,10 +178,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
-        Barryvdh\Debugbar\ServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
-
+        
     ],
 
     /*
